27e4d048d75ee9d5f09b5e3df1cdf492359ea432
NicoSB <nico.strebel@uzh.ch>
2016-07-18 11:31:53 +0200

    http::post agains

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -56,7 +56,7 @@ class ArticlesController < ApplicationController
 			#res = https.post2(uri.path, @to_send, headers)
 			request = Net::HTTP::Post.new(uri.path, initheader = headers)
 
-			request.body = "[ #{@to_send} ]"
+			request.body = @to_send
 			puts "JSON: " + @to_send
 
 			puts "----------------- send notification"
