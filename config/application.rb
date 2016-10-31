8f828f8626102fb500e8a92ec10eda1fd8e3212c
NicoSB <ncstrbl@gmail.com>
2016-10-31 12:00:24 +0100

    ênable jobs, stupid fuck

--- config/application.rb
+++ config/application.rb
@@ -25,9 +25,9 @@ module EhcofanServer
     config.active_record.raise_in_transactional_callbacks = true
   end
   Rails.application.config.after_initialize do
-    #FetchArticlesJob.perform_async()
-   # FetchMatchesJob.perform_async()
-   # FetchTeamsJob.perform_async()
+    FetchArticlesJob.perform_async()
+    FetchMatchesJob.perform_async()
+    FetchTeamsJob.perform_async()
     FetchPlayerStatsJob.perform_async()
   end
   Rails.application.config.before_configuration do
