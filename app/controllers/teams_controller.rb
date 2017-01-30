adf141c56586e62c7924211ec331b770aa726ee4
NicoSB <ncstrbl@gmail.com>
2017-01-30 20:58:00 +0100

    small fix for teams_controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,8 +4,11 @@ class TeamsController < ApplicationController
 	def index
 		if(params[:mode] != nil && params[:mode] == "control")
 			@teams = Team.all
-		else
+		elsif(params[:competition] != nil && params[:competition] != "")
 			@teams = Team.where(competition: params[:competition]).order("teams.group ASC, wins*3 + ot_wins*2 + ot_losses DESC, goals_for - goals_against DESC , goals_for DESC")
+			render :json => @teams, :except=>[:updated_at, :created_at]
+		else
+			@teams = Team.order("teams.group ASC, wins*3 + ot_wins*2 + ot_losses DESC, goals_for - goals_against DESC , goals_for DESC")
 			render :json => @teams, :except=>[:updated_at, :created_at]
 		end
 	end
