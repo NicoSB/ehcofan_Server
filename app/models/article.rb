2ac468abd5ebc82716a2bf85f12f005294c0c446
NicoSB <ncstrbl@gmail.com>
2017-01-30 17:57:00 +0100

    disable notifications temporarily

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,7 @@ class Article < ActiveRecord::Base
 	has_attached_file :news_image, :default_url => "/images/default.jpg"
 	validates_attachment :news_image, content_type: { content_type: /\Aimage\/.*\Z/ }
 
-	after_create :send_notification, on: :create
+	#after_create :send_notification, on: :create
 
 	private
 		def send_notification
